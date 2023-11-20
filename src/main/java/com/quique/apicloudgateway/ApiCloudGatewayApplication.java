e10e063f31ac070a3bb67adfb6578e65d1a20dfd
Quique <ncnr_quique@hotmail.com>
2023-11-20 14:55:44 -0500

    spring cloud

---
+++ src/main/java/com/quique/apicloudgateway/ApiCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.quique.apicloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCloudGatewayApplication.class, args);
+	}
+
+}
